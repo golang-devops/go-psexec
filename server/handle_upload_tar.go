c64f74686762dab13c430b5d85240922739e9115
Francois Hill (home win10) <francoishill11@gmail.com>
2016-04-30 14:13:52 +0200

    Auto determine if download tar is file or dir

--- server/handle_upload_tar.go
+++ server/handle_upload_tar.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/labstack/echo"
@@ -17,7 +18,18 @@ func (h *handler) handleUploadTarFunc(c *echo.Context) error {
 		return fmt.Errorf("Request does not contain header '%s'", shared.BASE_PATH_HTTP_HEADER_NAME)
 	}
 
-	h.logger.Infof("Now starting to receive dir '%s'", basePath)
-	tarReceiver := tar_io.Factories.TarReceiver.Dir(basePath)
-	return tar_io.SaveTarToReceiver(req.Body, tarReceiver)
+	info, err := os.Stat(basePath)
+	if err != nil {
+		return fmt.Errorf("Unable to obtain stats of path '%s', error: %s", basePath, err.Error())
+	}
+
+	if info.IsDir() {
+		h.logger.Infof("Now starting to receive dir '%s'", basePath)
+		tarReceiver := tar_io.Factories.TarReceiver.Dir(basePath)
+		return tar_io.SaveTarToReceiver(req.Body, tarReceiver)
+	} else {
+		h.logger.Infof("Now starting to receive file '%s'", basePath)
+		tarReceiver := tar_io.Factories.TarReceiver.File(basePath)
+		return tar_io.SaveTarToReceiver(req.Body, tarReceiver)
+	}
 }
