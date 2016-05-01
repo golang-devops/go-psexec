393651d0ee674be3d43be0e48a29a93230d1382e
Francois Hill (home win10) <francoishill11@gmail.com>
2016-05-01 08:58:17 +0200

    Bump to version 0.0.5

--- server/main.go
+++ server/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	TempVersion      = "0.0.4" //Until we integrate with travis
+	TempVersion      = "0.0.5" //Until we integrate with travis
 	CURRENT_USER_VAL = "use_current"
 )
 
